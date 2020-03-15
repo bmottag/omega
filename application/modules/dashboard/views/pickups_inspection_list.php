b318aed3cb56fb2f3cf10acffdc308ed56100128
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2020-03-15 12:31:19 -0600

    Dashboard: Se adiciona estilos para los registros que tienen comentarios, o tienen algun registro con falla.

--- application/modules/dashboard/views/pickups_inspection_list.php
+++ application/modules/dashboard/views/pickups_inspection_list.php
@@ -30,19 +30,70 @@
 						<tbody>							
 						<?php
 							foreach ($infoDaily as $lista):
-								echo "<tr>";
-								echo "<td class='text-center'>" . $lista['date_issue'] . "</td>";
+							
+$class = "";
+								
+//si hay errores se coloca en amarillo
+if(
+$lista['belt'] == 0
+ || $lista['power_steering'] == 0
+ || $lista['oil_level'] == 0
+ || $lista['coolant_level'] == 0
+ || $lista['water_leaks'] == 0
+ || $lista['nuts'] == 0
+ || $lista['head_lamps'] == 0
+ || $lista['hazard_lights'] == 0
+ || $lista['clearance_lights'] == 0
+ || $lista['bake_lights'] == 0
+ || $lista['work_lights'] == 0
+ || $lista['glass'] == 0
+ || $lista['clean_exterior'] == 0
+ || $lista['proper_decals'] == 0
+ || $lista['brake_pedal'] == 0
+ || $lista['emergency_brake'] == 0
+ || $lista['backup_beeper'] == 0
+ || $lista['beacon_light'] == 0
+ || $lista['gauges'] == 0
+ || $lista['horn'] == 0
+ || $lista['hoist'] == 0
+ || $lista['passenger_door'] == 0
+ || $lista['seatbelts'] == 0
+ || $lista['fire_extinguisher'] == 0
+ || $lista['emergency_reflectors'] == 0
+ || $lista['first_aid'] == 0
+ || $lista['wipers'] == 0
+ || $lista['drives_axle'] == 0
+ || $lista['grease_front'] == 0
+ || $lista['grease_end'] == 0
+ || $lista['spill_kit'] == 0
+ || $lista['grease'] == 0
+ || $lista['steering_axle'] == 0
+ || $lista['turn_signals'] == 0
+ || $lista['clean_interior'] == 0
+ || $lista['insurance'] == 0
+ || $lista['driver_seat'] == 0
+ || $lista['registration'] == 0){
+	$class = "warning";
+}
+
+//si hay comentarios se coloca en rojo
+if($lista['comments'] != ''){
+	$class = "danger";
+}
+							
+								echo "<tr class='" . $class . "'>";
+								echo "<td class='text-center'><p class='text-" . $class . "'>" . $lista['date_issue'] . "</p></td>";
 								echo "<td class='text-center'>";
 						?>
 <a href='<?php echo base_url('report/generaInsectionDailyPDF/x/x/x/x/x/' . $lista['id_inspection_daily'] ); ?>' target="_blank"> <img src='<?php echo base_url_images('pdf.png'); ?>' ></a>
 						<?php
 								echo "</td>";
-								echo "<td>" . $lista['name'] . "</td>";
-								echo "<td class='text-center'>" . $lista['make'] . "</td>";
-								echo "<td class='text-center'>" . $lista['model'] . "</td>";
-								echo "<td class='text-center'>" . $lista['unit_number'] . "</td>";
-								echo "<td >" . $lista['description'] . "</td>";
-								echo "<td>" . $lista['comments'] . "</td>";
+								echo "<td><p class='text-" . $class . "'>" . $lista['name'] . "</p></td>";
+								echo "<td class='text-center'><p class='text-" . $class . "'>" . $lista['make'] . "</p></td>";
+								echo "<td class='text-center'><p class='text-" . $class . "'>" . $lista['model'] . "</p></td>";
+								echo "<td class='text-center'><p class='text-" . $class . "'>" . $lista['unit_number'] . "</p></td>";
+								echo "<td><p class='text-" . $class . "'>" . $lista['description'] . "</p></td>";
+								echo "<td><p class='text-" . $class . "'>" . $lista['comments'] . "</p></td>";
 								echo "</tr>";
 							endforeach;
 						?>
