21997d8eca9f5abbc7c7b69c650e8f4fe83f3461
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2020-04-01 15:29:10 -0600

    Dashboard-Genera info: Se ajusto la informacion de los permisos por roles

--- application/modules/dashboard/views/general_info.php
+++ application/modules/dashboard/views/general_info.php
@@ -14,14 +14,15 @@
 * <strong>Dashboard:</strong> payroll solo sus registros, inspecciones solo sus inspecciones
 <br><strong>Record Task:</strong> Payroll
 <br><strong>Record Task:</strong> Hauling
-<br><strong>Incidences</strong>
+<br><strong>Incidences:</strong> Near miss report
+<br><strong>Incidences:</strong> Incident/Accident report
 <br><strong>Day off</strong> (Ask for)
 <br>----------------------------
 <br>* <strong>Work order:</strong>
 <ul>
+<li>Add/Edit</li>
 <li>Pueden solo crear workorders y ver las que crearon</li>
 <li>No puede cambiar estado de la work order</li>
-<li>No puede tener acceso a enlace search ni search income</li>
 <li>Quitar boton save and send to the client</li>
 <li>Quitar boton asign rate ----perfil work order y acounting</li>
 </ul>
@@ -45,21 +46,22 @@
 * <strong>Dashboard: </strong> payroll todos los registros, todas las inspecciones
 <br><strong>Record Task:</strong> Payroll
 <br><strong>Record Task:</strong> Hauling
-<br><strong>Incidences</strong>
+<br><strong>Incidences:</strong> Near miss report
+<br><strong>Incidences:</strong> Incident/Accident report
 <br><strong>Day off</strong> (Ask for)
 <br>----------------------------
 <br>* <strong>Work order:</strong>
 <ul>
-<li>Puede buscar work orders</li>
+<li>Add/Edit</li>
+<li>Search</li>
 <li>Puede editar cualquiera simpere y cuando este en on field</li>
 <li>Si esta en otro estado solo la puede ver no la puede editar</li>
 <li>No puede aign rate</li>
 <li>No pueden descargar invoice</li>
-<li>En settings puede ir a planning</li>
-<li>Reportes no tiene acceso</li>
-<li>No tienen accesos a ppe inspeccion</li>
 </ul>
 ----------------------------
+<br><strong>Settings:</strong> Planning
+<br>----------------------------
 <br><strong>Manuals</strong>
 					</p>
 				</div>
@@ -76,12 +78,17 @@
 				</div>
 				<div class="panel-body">
 					<p>
-<strong>Record Task:</strong> Payroll
-<br><strong>Incidences</strong>
+* <strong>Dashboard: </strong>timestam, lashauling recors, le quito safety y le quito inspecciones 
+<br><strong>Record Task:</strong> Payroll
+<br><strong>Incidences:</strong> Near miss report
+<br><strong>Incidences:</strong> Incident/Accident report
 <br><strong>Day off</strong> (Ask for)
 <br>----------------------------
 <br>* <strong>Work order:</strong>
 <ul>
+<li>Add/Edit</li>
+<li>Search</li>
+<li>Search Income</li>
 <li>CAMBIA ESTADO A REVISADA</li>
 <li>Workorders el estado solo puede cambiar de on field a in progress y de in progress a revised</li>
 <li>Si esta revisada solo la puede ver no la puede editar</li>
@@ -90,7 +97,8 @@
 <br><strong>Reports:</strong> Payroll Report
 <br><strong>Reports:</strong> Hauling Report
 <br><strong>Reports:</strong> Work Order Report
-
+----------------------------
+<br><strong>Manuals</strong>
 					</p>
 				</div>
 				<div class="panel-footer">
@@ -114,23 +122,27 @@
 * <strong>Dashboard: </strong>Le muestro notificaciones de futuros mantenimientos
 <br><strong>Record Task:</strong> Payroll
 <br><strong>Record Task:</strong> PPE inspection
-<br>* Jobs info
+<br>* <strong>Jobs info</strong>
 <br>----------------------------
 <br>En jha solo dar permisos a safety
 <br>----------------------------
-<br><strong>Incidences</strong>
+<br><strong>Incidences:</strong> Near miss report
+<br><strong>Incidences:</strong> Incident/Accident report
 <br><strong>Day off</strong> (Ask for)
 <br>----------------------------
 <br>* <strong>Work order:</strong>
 <ul>
+<li>Add/Edit</li>
 <li>Pueden solo crear workorders y ver las que crearon</li>
 <li>No puede cambiar estado de la work order</li>
-<li>No puede tener acceso a enlace search ni search income</li>
 <li>Quitar boton save and send to the client</li>
 <li>Quitar boton asign rate ----perfil work order y acounting</li>
 </ul>
 ----------------------------
-<br><strong>Day off </strong>(Approval)
+<br><strong>Manage Day off: </strong> New Day Off
+<br><strong>Manage Day off: </strong> Approved Day Off
+<br><strong>Manage Day off: </strong> Denied Day Off
+<br>----------------------------
 <br><strong>Reports:</strong> Incidences Report
 <br><strong>Reports:</strong> Maintenace Report
 <br><strong>Reports:</strong> FLHA Report
@@ -142,6 +154,7 @@
 <br><strong>Settings:</strong> Hazard
 <br><strong>Settings:</strong> Hazard activity
 <br><strong>Settings:</strong> Vehicles
+<br><strong>Settings:</strong> Rentals
 <br><strong>Settings:</strong> Link to videos
 <br><strong>Settings:</strong> Link to manuals
 <br>----------------------------
@@ -164,11 +177,14 @@
 					<p>
 * <strong>Dashboard: </strong>timestam, lashauling recors, le quito safety y le quito inspecciones
 <br><strong>Record Task:</strong> Payroll
-<br><strong>Incidences</strong>
-<br><strong>Day off</strong> (Ask for)
+<br><strong>Incidences:</strong> Near miss report
+<br><strong>Incidences:</strong> Incident/Accident report
 <br>----------------------------
 <br>* <strong>Work order:</strong>
 <ul>
+<li>Add/Edit</li>
+<li>Search</li>
+<li>Search Income</li>
 <li>Todo work orders</li>
 <li>Solo puede editar work orders que estan en revised, sento to the client y close, de resto solo las ve</li>
 </ul>
@@ -183,10 +199,11 @@
 <br><strong>Reports:</strong> Construction Equipment Inspection Report
 <br><strong>Reports:</strong> Work Order Report
 <br>----------------------------
-<br><strong>Settings:</strong> Planning ---> solo puede ver, no puede modificar
 <br><strong>Settings:</strong> Company
-<br><strong>Settings:</strong> Vehicles ---> de mantenimiento solo puede verlo, no editarlo ni adicionar
-
+<br>* <strong>Settings:</strong> Vehicles ---> solo puede ver, no puede modificar
+<br>* <strong>Settings:</strong> Rentals ---> solo puede ver, no puede modificar
+<br>----------------------------
+<br><strong>Manuals</strong>
 					</p>
 				</div>
 				<div class="panel-footer">
@@ -208,18 +225,31 @@
 <br>----------------------------
 <br>* <strong>Work order:</strong>
 <ul>
+<li>Add/Edit</li>
+<li>Search</li>
+<li>Search Income</li>
 <li>Todo work orders</li>
-<li>Fabian me especifica permisos en esta parte</li>
+<li>Solo puede editar work orders que estan en revised, sento to the client y close, de resto solo las ve</li>
 </ul>
 ----------------------------
+<br><strong>Reports:</strong> Payroll Report
 <br><strong>Reports:</strong> Incidences Report
+<br><strong>Reports:</strong> Maintenace Report
+<br><strong>Reports:</strong> FLHA Report
+<br><strong>Reports:</strong> Hauling Report
+<br><strong>Reports:</strong> Hauling Report
 <br><strong>Reports:</strong> Pickups & Trucks Inspection Report
 <br><strong>Reports:</strong> Construction Equipment Inspection Report
 <br><strong>Reports:</strong> Work Order Report
 <br>----------------------------
-<br><strong>Settings:</strong> Employee ---> solo puede ver, no puede modificar
-<br><strong>Settings:</strong> Vehicles ---> solo puede ver, no puede modificar
-
+<br>* <strong>Settings:</strong> Employee ---> solo puede ver, no puede modificar
+<br><strong>Settings:</strong> Job Code/Name
+<br><strong>Settings:</strong> Planning
+<br><strong>Settings:</strong> Company
+<br>* <strong>Settings:</strong> Vehicles ---> solo puede ver, no puede modificar
+<br>* <strong>Settings:</strong> Rentals ---> solo puede ver, no puede modificar
+<br>----------------------------
+<br><strong>Manuals</strong>
 					</p>
 				</div>
 				<div class="panel-footer">
