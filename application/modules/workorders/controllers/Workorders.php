49eff3dec39ea48e37e21095a5e00e113701bbab
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2020-04-13 17:18:21 -0600

    Work order: Se agrega libreria para generar documento en excel

--- application/modules/workorders/controllers/Workorders.php
+++ application/modules/workorders/controllers/Workorders.php
@@ -6,6 +6,7 @@ class Workorders extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->model("workorders_model");
+		$this->load->library('PHPExcel.php');
     }
 	
 	/**
