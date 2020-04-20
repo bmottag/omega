4879d79a002dfcb7bee5aad899e8a0547628d6c6
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2020-04-20 08:03:03 -0600

    Libreria reportico

---
+++ public/reportico/projects/Accident/menu.php
@@ -0,0 +1,25 @@
+<?php
+$menu_title = SW_PROJECT_TITLE;
+$menu = array (
+	array ( "language" => "en_gb", "report" => ".*\.xml", "title" => "<AUTO>" )
+	);
+?>
+<?php
+$menu_title = SW_PROJECT_TITLE;
+$menu = array (
+	array ( "report" => "<p>In the top menu you can find all the reports that you can view on the screen or download to PDF or CSV.</p><p><a style=\"text-decoration: underline !important\"  target=\"_self\" href=\"https://v-contracting.ca/app/dashboard\">Go back</a></p>", "title" => "TEXT" ),
+	);
+
+$admin_menu = $menu;
+
+$dropdown_menu = array(
+                    array ( 
+                        "project" => "Accident",
+                        "title" => "Incidences",
+                        "items" => array (
+							array ( "reportfile" => "near_miss.xml" ),
+							array ( "reportfile" => "accident.xml" )
+                            )
+                        ),
+                );
+?>
