844f0a0215c6a8e9c5281e35bdc5eea6d2eb4ac6
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2020-10-22 13:51:15 -0600

    Reportes: Se adiciona tiempo para generar reportes en PDF

--- application/modules/report/controllers/Report.php
+++ application/modules/report/controllers/Report.php
@@ -270,7 +270,7 @@ class Report extends CI_Controller {
 	public function generaSafetyPDF($jobId, $from, $to, $idSafety='x')
 	{
 			$this->load->library('Pdf');
-			
+set_time_limit(60);			
 			// create new PDF document
 			$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
