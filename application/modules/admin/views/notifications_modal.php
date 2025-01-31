e1e1b3a472ff6b453caf26832a3e266f5a026806
Fabiana Orozco <sfabi268@gmail.com>
2025-01-31 19:21:01 -0400

    clean code

--- application/modules/admin/views/notifications_modal.php
+++ application/modules/admin/views/notifications_modal.php
@@ -2,22 +2,24 @@
 <div class="modal-header">
 	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 	<h4 class="modal-title" id="exampleModalLabel">Notifications Access Settings Form
-	<br><small>Add/Edit Notifications Access</small>
+		<br><small>Add/Edit Notifications Access</small>
 	</h4>
 </div>
 
 <div class="modal-body">
-	<form name="form" id="form" role="form" method="post" >
-		<input type="hidden" id="hddId" name="hddId" value="<?php echo $information?$information[0]["id_notification_access"]:""; ?>"/>
-		
+	<form name="form" id="form" role="form" method="post">
+		<input type="hidden" id="hddId" name="hddId" value="<?php echo $information ? $information[0]["id_notification_access"] : ""; ?>" />
+
 		<div class="row">
-			<div class="col-sm-6">		
+			<div class="col-sm-6">
 				<div class="form-group text-left">
 					<label class="control-label" for="notification">Notification: *</label>
-					<select name="notification" id="notification" class="form-control" >
+					<select name="notification" id="notification" class="form-control">
 						<option value=''>Select...</option>
 						<?php for ($i = 0; $i < count($notificationsList); $i++) { ?>
-							<option value="<?php echo $notificationsList[$i]["id_notification"]; ?>" <?php if($information && $information[0]["fk_id_notification"] == $notificationsList[$i]["id_notification"]) { echo "selected"; }  ?>><?php echo $notificationsList[$i]["notification"]; ?></option>	
+							<option value="<?php echo $notificationsList[$i]["id_notification"]; ?>" <?php if ($information && $information[0]["fk_id_notification"] == $notificationsList[$i]["id_notification"]) {
+																											echo "selected";
+																										}  ?>><?php echo $notificationsList[$i]["notification"]; ?></option>
 						<?php } ?>
 					</select>
 				</div>
@@ -25,53 +27,57 @@
 		</div>
 
 		<div class="row">
-			<div class="col-sm-6">		
+			<div class="col-sm-6">
 				<div class="form-group text-left">
 					<label class="control-label" for="emailTo">Send Email to:</label>
-					<select name="emailTo" id="emailTo" class="form-control" >
+					<select name="emailTo" id="emailTo" class="form-control">
 						<option value=''>Select...</option>
 						<?php for ($i = 0; $i < count($workersList); $i++) { ?>
-							<option value="<?php echo $workersList[$i]["id_user"]; ?>" <?php if($information && $information[0]["fk_id_user_email"] == $workersList[$i]["id_user"]) { echo "selected"; }  ?>><?php echo $workersList[$i]["first_name"] . ' ' . $workersList[$i]["last_name"]; ?></option>	
+							<option value="<?php echo $workersList[$i]["id_user"]; ?>" <?php if ($information && $information[0]["fk_id_user_email"] == $workersList[$i]["id_user"]) {
+																							echo "selected";
+																						}  ?>><?php echo $workersList[$i]["first_name"] . ' ' . $workersList[$i]["last_name"]; ?></option>
 						<?php } ?>
 					</select>
 				</div>
 			</div>
-			
+
 			<div class="col-sm-6">
 				<div class="form-group text-left">
 					<label class="control-label" for="smsTo">Send SMS to:</label>
-					<select name="smsTo" id="smsTo" class="form-control" >
+					<select name="smsTo" id="smsTo" class="form-control">
 						<option value=''>Select...</option>
 						<?php for ($i = 0; $i < count($workersList); $i++) { ?>
-							<option value="<?php echo $workersList[$i]["id_user"]; ?>" <?php if($information && $information[0]["fk_id_user_sms"] == $workersList[$i]["id_user"]) { echo "selected"; }  ?>><?php echo $workersList[$i]["first_name"] . ' ' . $workersList[$i]["last_name"]; ?></option>	
+							<option value="<?php echo $workersList[$i]["id_user"]; ?>" <?php if ($information && $information[0]["fk_id_user_sms"] == $workersList[$i]["id_user"]) {
+																							echo "selected";
+																						}  ?>><?php echo $workersList[$i]["first_name"] . ' ' . $workersList[$i]["last_name"]; ?></option>
 						<?php } ?>
 					</select>
 				</div>
 			</div>
 		</div>
-		
+
 		<div class="form-group">
-			<div id="div_load" style="display:none">		
+			<div id="div_load" style="display:none">
 				<div class="progress progress-striped active">
 					<div class="progress-bar" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
 						<span class="sr-only">45% completado</span>
 					</div>
 				</div>
 			</div>
-			<div id="div_error" style="display:none">			
+			<div id="div_error" style="display:none">
 				<div class="alert alert-danger"><span class="glyphicon glyphicon-remove" id="span_msj">&nbsp;</span></div>
-			</div>	
+			</div>
 		</div>
 
 		<div class="form-group">
 			<div class="row" align="center">
 				<div style="width:50%;" align="center">
-					<button type="button" id="btnSubmit" name="btnSubmit" class="btn btn-primary" >
+					<button type="button" id="btnSubmit" name="btnSubmit" class="btn btn-primary">
 						Save <span class="glyphicon glyphicon-floppy-disk" aria-hidden="true">
-					</button> 
+					</button>
 				</div>
 			</div>
 		</div>
-			
+
 	</form>
 </div>
