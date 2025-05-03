ae91e5467eedf81d731ef5032073af9564d657c0
bmottag <benmotta@gmail.com>
2025-05-03 08:17:29 -0500

    Disabled button to elimnate duplicate entris

--- application/modules/payroll/views/form_end_payroll.php
+++ application/modules/payroll/views/form_end_payroll.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="<?php echo base_url("assets/js/validate/payroll/payrollStart.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/validate/payroll/payrollStart.js?v=2"); ?>"></script>
 
 <script>
 	$(document).ready(function() {
